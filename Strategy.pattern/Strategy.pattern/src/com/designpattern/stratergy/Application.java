dee71abaeaf8d5d4fc8d2e3dcc83a9a840e9d54e
unknown <wrchamara89@gmail.com>
2015-11-21 16:07:18 +0530

    Add Strategy pattern codebase

---
+++ Strategy.pattern/Strategy.pattern/src/com/designpattern/stratergy/Application.java
@@ -0,0 +1,18 @@
+/**
+ * Application.java
+ * 
+ * @Author: Chamara Madusanka Jayalath
+ * 
+ * Created Date: Nov 21, 2015
+ * 
+ * Object oriented design patterns
+ */
+package com.designpattern.stratergy;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
